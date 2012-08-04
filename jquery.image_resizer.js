07e51548c6c170c8b35515e9283fecaf43d9542d
Rajat Saxena <rajat.saxena.work@gmail.com>
2012-08-04 13:18:46 +0530

    update  plugin file with license attached

--- jquery.image_resizer.js
+++ jquery.image_resizer.js
@@ -1,3 +1,23 @@
+/*
+* Image Resizer jQuery Plugin
+* adjust overflowing images by resizing them.
+* 
+* Copywrite (c) 2012  Rajat Saxena<rajat.saxena.work@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 (function($){
 	$.fn.image_resizer = function(options){
 		$.fn.image_resizer.defaults = { MAX_HEIGHT : 280,MAX_WIDTH : 330,BORDER_WIDTH : 10 }
